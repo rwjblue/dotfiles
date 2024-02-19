d59204a811f3464a20d794a1ee51606bdc24507b
Robert Jackson <me@rwjblue.com>
2024-02-19 09:28:23 -0500

    Ensure that `gz` works with lazily loaded zoxide
    This fixes two different issues:
    
    1. After the refactor from fasd to zoxide, I could no longer `gz foo`
       when I was within the `foo` directory already. That's because `z`
       (zoxides shell function for this) specifically excludes the current
       working directory (it does `zoxide query $@ --exclude $PWD).
    2. After refactoring to use `sheldon` and `zsh-defer` the `tmux.zsh`
       file was being loaded before the `zoxide.zsh` file, which meant that
       the `z` shell function wasn't defined when `gz` was being defined.
    
    The fix here just avoids using the `z` shell function, and uses `zoxide`
    directly.

--- zsh/plugins/tmux.zsh
+++ zsh/plugins/tmux.zsh
@@ -17,11 +17,11 @@ __ts() {
   fi
 }
 
-# use `z` (from zoxide setup above) to switch to a directory, if successful
-# rename the window to the current working directory; this is generally nice
-# when that dir is the name of a specific repo
+# use `z` (from zoxide) to switch to a directory, if successful rename the
+# window to the current working directory; this is generally nice when that dir
+# is the name of a specific repo
 function __tz {
-  z $@ && tmux rename-window  "${PWD##*/}"
+  local result="$(zoxide query -- "$@")" && cd "$result" && tmux rename-window "${PWD##*/}"
 }
 
 alias t='tmux'
