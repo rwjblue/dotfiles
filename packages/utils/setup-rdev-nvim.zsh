76a8e0d8e9c27e84a6fd0178eedbe31629e0bbeb
Robert Jackson <rjackson@linkedin.com>
2024-06-12 15:36:12 -0400

    fix(nvim-install): Don't install neovim is $HOME/neovim is already setup

--- packages/utils/setup-rdev-nvim.zsh
+++ packages/utils/setup-rdev-nvim.zsh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 
+if [[ -d $HOME/neovim ]]; then
+    echo "$HOME/neovim already exists, doing nothing"
+    exit 0
+fi
+
+# existing script continues here...
 echo "installing neovim"
 echo "initial nvim version"
 nvim --version
