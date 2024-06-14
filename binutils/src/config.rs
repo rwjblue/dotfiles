914772a5685f21933ab0b20c13403c4aeb71d3ff
Robert Jackson <rjackson@linkedin.com>
2024-06-14 17:58:42 -0400

    Extract shared test utils to separate file

--- binutils/src/config.rs
+++ binutils/src/config.rs
@@ -117,50 +117,10 @@ fn revert_tokens_in_path(path: &Path) -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::test_utils::setup_test_environment;
     use insta::{assert_snapshot, assert_toml_snapshot};
     use std::env;
     use tempfile::tempdir;
-
-    #[derive(Debug, Clone)]
-    struct TestEnvironment {
-        _home: PathBuf,
-        _config_dir: PathBuf,
-        config_file: PathBuf,
-        original_home: Option<String>,
-    }
-
-    impl Drop for TestEnvironment {
-        fn drop(&mut self) {
-            match &self.original_home {
-                Some(home) => env::set_var("HOME", home),
-                None => env::remove_var("HOME"),
-            }
-        }
-    }
-
-    fn setup_test_environment() -> TestEnvironment {
-        let original_home = env::var("HOME").ok();
-        let temp_dir = tempdir().expect("Failed to create temp dir");
-        let temp_home = temp_dir.into_path();
-
-        env::set_var(
-            "HOME",
-            temp_home
-                .to_str()
-                .expect("Failed to convert temp path to str"),
-        );
-
-        // Ensure config directory exists
-        let config_dir = temp_home.join(".config/binutils");
-        fs::create_dir_all(&config_dir).expect("Failed to create .config directory");
-
-        TestEnvironment {
-            _home: temp_home,
-            config_file: config_dir.join("config.toml"),
-            _config_dir: config_dir,
-            original_home,
-        }
-    }
 
     #[test]
     fn test_replace_tokens_in_path_with_home() {
--- binutils/src/lib.rs
+++ binutils/src/lib.rs
@@ -1,2 +1,5 @@
 pub mod config;
 pub mod tmux;
+
+#[cfg(test)]
+pub mod test_utils;
