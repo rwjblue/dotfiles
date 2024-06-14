160e108bdf83b4817aa757cac97355e9baf6ce60
Robert Jackson <rjackson@linkedin.com>
2024-06-14 17:58:59 -0400

    Add basic test for `in_tmux`

--- binutils/src/tmux.rs
+++ binutils/src/tmux.rs
@@ -1,3 +1,27 @@
 pub fn in_tmux() -> bool {
     std::env::var("TMUX").is_ok()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_utils::setup_test_environment;
+
+    #[test]
+    fn test_in_tmux_within_tmux() {
+        temp_env::with_var(
+            "TMUX",
+            Some("/private/tmp/tmux-23547/default,39774,0"),
+            || {
+                assert!(in_tmux());
+            },
+        );
+    }
+
+    #[test]
+    fn test_in_tmux_outside_tmux() {
+        temp_env::with_var("TMUX", None::<String>, || {
+            assert!(!in_tmux());
+        });
+    }
+}
