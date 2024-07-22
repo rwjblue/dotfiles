d740924f10f2935c037cd81e6915f66d4c561001
Robert Jackson <rjackson@linkedin.com>
2024-07-22 11:50:33 -0400

    Move most zsh setup into sheldon plugins

--- zsh/plugins/fzf.zsh
+++ zsh/plugins/fzf.zsh
@@ -3,10 +3,10 @@
 
 if [[ "$OSTYPE" == darwin* ]]; then
   # Auto-completion
-  [[ $- == *i* ]] && source "$HOMEBREW_PREFIX/opt/fzf/shell/completion.zsh" 2> /dev/null
+  [[ $- == *i* ]] && source "/opt/homeberw/opt/fzf/shell/completion.zsh" 2> /dev/null
 
   # Key bindings
-  source "$HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.zsh"
+  source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 else
   if [[ ! "$PATH" == */$HOME/.fzf/bin* ]]; then
     export PATH="$PATH:$HOME/.fzf/bin"
