71cc5945ad5daa4794ea570cf77f0dbb9cce3a97
Robert Jackson <rjackson@linkedin.com>
2024-07-22 11:50:33 -0400

    Move most zsh setup into sheldon plugins

---
+++ packages/zsh/plugins/basic_config.zsh
@@ -0,0 +1,12 @@
+# Setup vi Mode
+# -------------
+bindkey -v
+export KEYTIMEOUT=1
+
+export LANG='en_US.UTF-8'
+
+# use nvim as default editor
+export EDITOR="nvim"
+
+export PATH="$PATH:/opt/homebrew/opt/fzf/bin"
+
