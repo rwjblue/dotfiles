9f963f671a36605fb90b8f87f15cd9a07c70669a
Robert Jackson <rjackson@linkedin.com>
2024-08-09 13:44:37 -0400

    Make brew setup eager (but don't eval `brew shellenv`, just vendor it)

---
+++ zsh/plugins/brew.zsh
@@ -0,0 +1,7 @@
+# regenerate with `brew shellenv` if we have any homebrew issues
+export HOMEBREW_PREFIX="/opt/homebrew";
+export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
+export HOMEBREW_REPOSITORY="/opt/homebrew";
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
+[ -z "${MANPATH-}" ] || export MANPATH=":${MANPATH#:}";
+export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
--- zsh/plugins_deferred/brew.zsh
+++
@@ -1,3 +0,0 @@
-if [ -x "/opt/homebrew/bin/brew" ]; then
-  eval $(/opt/homebrew/bin/brew shellenv)
-fi
