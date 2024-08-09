7717582e6ffbffe4277b2768af55f855d2e72a28
Robert Jackson <rjackson@linkedin.com>
2024-08-09 13:54:46 -0400

    Get rid of pyenv slowdown

--- zsh/plugins_deferred/pyenv.zsh
+++ zsh/plugins_deferred/pyenv.zsh
@@ -1,4 +1,14 @@
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+# pyenv is _sooooo_ slow so we only want to run it when we actually need it
+function pyenv_init() {
+  export PYENV_ROOT="$HOME/.pyenv"
+  [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+
+  unalias pyenv
+
+  eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
+
+  echo "pyenv initialized; re-run your command"
+}
+
+alias pyenv=pyenv_init
