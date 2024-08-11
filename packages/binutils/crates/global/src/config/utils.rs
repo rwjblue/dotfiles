0d41902b8c0cb1779d2871e296120c306c514b29
Robert Jackson <me@rwjblue.com>
2024-08-11 10:55:32 -0400

    Refactor config into a few submodules

--- packages/binutils/crates/global/src/config/utils.rs
+++ packages/binutils/crates/global/src/config/utils.rs
@@ -6,50 +6,11 @@ use std::path::{Path, PathBuf};
 use tracing::{debug, error, trace};
 
 use anyhow::{Context, Result};
-use serde::{Deserialize, Serialize};
 use toml;
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct Config {
-    pub tmux: Option<Tmux>,
-}
+use crate::config::default_config;
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct Tmux {
-    pub sessions: Vec<Session>,
-}
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct Session {
-    pub name: String,
-    pub windows: Vec<Window>,
-}
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(untagged)]
-pub enum Command {
-    Single(String),
-    Multiple(Vec<String>),
-}
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct Window {
-    pub name: String,
-    #[serde(
-        default,
-        serialize_with = "path_to_string",
-        deserialize_with = "string_to_path"
-    )]
-    pub path: Option<PathBuf>,
-    pub command: Option<Command>,
-}
-
-fn default_config() -> Config {
-    Config {
-        // TODO: make tmux optional
-        tmux: Some(Tmux { sessions: vec![] }),
-    }
-}
+use super::types::{expand_tilde, Config};
 
 pub fn get_config_file_path() -> PathBuf {
     let home_dir = env::var("HOME").expect("HOME environment variable not set");
@@ -70,20 +31,6 @@ pub fn write_config(config: &Config) -> Result<()> {
     let mut file = File::create(config_path)?;
     file.write_all(toml_str.as_bytes())?;
     Ok(())
-}
-
-fn expand_tilde(path: PathBuf) -> PathBuf {
-    let path_str = path.to_str().unwrap_or_default();
-
-    match path_str.strip_prefix("~") {
-        Some(stripped) => {
-            let home_dir = env::var("HOME").expect("HOME environment variable not set");
-            let expanded = format!("{}{}", home_dir, stripped);
-
-            PathBuf::from(expanded)
-        }
-        None => path,
-    }
 }
 
 pub fn read_config(config_path: Option<PathBuf>) -> Result<Config> {
@@ -142,122 +89,13 @@ pub fn read_config(config_path: Option<PathBuf>) -> Result<Config> {
     Ok(config)
 }
 
-pub fn path_to_string<S>(path: &Option<PathBuf>, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: serde::Serializer,
-{
-    match path {
-        Some(p) => {
-            let path_str = revert_tokens_in_path(p);
-            serializer.serialize_some(&path_str)
-        }
-        None => serializer.serialize_none(),
-    }
-}
-
-fn string_to_path<'de, D>(deserializer: D) -> Result<Option<PathBuf>, D::Error>
-where
-    D: serde::Deserializer<'de>,
-{
-    let opt = Option::<String>::deserialize(deserializer)?;
-    match opt {
-        Some(s) => Ok(Some(PathBuf::from(replace_tokens_in_path(&s)))),
-        None => Ok(None),
-    }
-}
-
-fn replace_tokens_in_path(path: &str) -> String {
-    let home_dir = env::var("HOME").unwrap_or_default();
-    path.replace("{home}", &home_dir)
-}
-
-fn revert_tokens_in_path(path: &Path) -> String {
-    let home_dir = env::var("HOME").unwrap_or_default();
-    let path_str = path.to_str().unwrap_or("");
-
-    if path_str.starts_with(&home_dir) {
-        path_str.replace(&home_dir, "{home}")
-    } else {
-        path_str.to_string()
-    }
-}
-
 #[cfg(test)]
 mod tests {
+    use super::super::types::{Command, Config, Session, Tmux, Window};
     use super::*;
     use crate::test_utils::{setup_test_environment, stabilize_home_paths};
     use insta::{assert_debug_snapshot, assert_snapshot, assert_toml_snapshot};
     use std::env;
-    use tempfile::tempdir;
-
-    #[test]
-    fn test_replace_tokens_in_path_with_home() {
-        let home_dir = env::var("HOME").expect("HOME not set");
-        let path = "{home}/some/path";
-        assert_eq!(
-            replace_tokens_in_path(path),
-            format!("{}/some/path", home_dir)
-        );
-    }
-
-    #[test]
-    fn test_revert_tokens_in_path_to_home() {
-        let home_dir = env::var("HOME").expect("HOME not set");
-        let path = PathBuf::from(format!("{}/some/path", home_dir));
-        assert_eq!(revert_tokens_in_path(&path), "{home}/some/path");
-    }
-
-    #[test]
-    fn test_replace_tokens_in_path_without_home() {
-        let path = "/some/other/path";
-        assert_eq!(replace_tokens_in_path(path), "/some/other/path");
-    }
-
-    #[test]
-    fn test_revert_tokens_in_path_without_home() {
-        let path = PathBuf::from("/some/other/path");
-        assert_eq!(revert_tokens_in_path(&path), "/some/other/path");
-    }
-
-    #[test]
-    fn test_replace_empty_path() {
-        assert_eq!(replace_tokens_in_path(""), "");
-    }
-
-    #[test]
-    fn test_revert_empty_path() {
-        let path = PathBuf::from("");
-        assert_eq!(revert_tokens_in_path(&path), "");
-    }
-
-    #[test]
-    fn test_path_just_home_token() {
-        let home_dir = env::var("HOME").expect("HOME not set");
-        assert_eq!(replace_tokens_in_path("{home}"), home_dir);
-    }
-
-    #[test]
-    fn test_path_just_home_directory() {
-        let home_dir = env::var("HOME").expect("HOME not set");
-        let path = PathBuf::from(&home_dir);
-        assert_eq!(revert_tokens_in_path(&path), "{home}");
-    }
-
-    // This test ensures that paths without the home directory are handled correctly
-    #[test]
-    fn test_temporary_directory_handling() {
-        let temp_dir = tempdir().expect("Failed to create a temporary directory");
-        let temp_path = temp_dir.path();
-        let temp_path_str = temp_path
-            .to_str()
-            .expect("Failed to convert temp path to str");
-
-        assert_eq!(
-            replace_tokens_in_path(temp_path_str),
-            temp_path_str,
-            "Temporary paths should not be altered if they do not contain the home directory."
-        );
-    }
 
     #[test]
     fn test_read_config_missing_file() {
