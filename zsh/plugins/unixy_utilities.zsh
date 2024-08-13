ee2e723e1f211a8bb5bf5ff836928cb11deaa341
Robert Jackson <rjackson@linkedin.com>
2024-08-13 17:54:39 -0400

    Fix completion with `eza` aliases
    Thank you @hjdivad

--- zsh/plugins/unixy_utilities.zsh
+++ zsh/plugins/unixy_utilities.zsh
@@ -4,9 +4,9 @@ alias lsof-tcp-listen='lsof -iTCP -sTCP:LISTEN -P'
 # use eza if it exists
 # https://github.com/eza-community/eza (replacement for https://github.com/ogham/exa)
 if which eza > /dev/null 2>&1; then
-  alias ls='eza --color-scale'
-  alias ll='eza -l --all --no-user --changed --sort=modified --color-scale'
-  alias lt='eza --tree --level 3 -l --no-permissions --no-user --no-time --color-scale'
+  alias ls='eza'
+  alias ll='eza -l --all --no-user --changed --sort=modified'
+  alias lt='eza --tree --level 3 -l --no-permissions --no-user --no-time'
 else
   # setup ls
   ls / --color=auto > /dev/null 2>&1
