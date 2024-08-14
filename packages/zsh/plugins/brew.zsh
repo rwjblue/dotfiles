7f9ecdfc71ff811fa18335afca669312feb48409
Robert Jackson <rjackson@linkedin.com>
2024-08-14 18:10:47 -0400

    Remove inlined generated output

--- packages/zsh/plugins/brew.zsh
+++ packages/zsh/plugins/brew.zsh
@@ -1,10 +1,1 @@
 # CMD: brew shellenv
-# OUTPUT START: brew shellenv
-export HOMEBREW_PREFIX="/opt/homebrew";
-export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
-export HOMEBREW_REPOSITORY="/opt/homebrew";
-export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
-[ -z "${MANPATH-}" ] || export MANPATH=":${MANPATH#:}";
-export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
-
-# OUTPUT END: brew shellenv
