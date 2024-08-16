2412f592385eeb46b176820cac7721a5548d7f73
Robert Jackson <rjackson@linkedin.com>
2024-08-16 15:53:35 -0400

    Add a bunch of TODO's (from session with @hjdivad)

--- binutils/crates/config/src/lib.rs
+++ binutils/crates/config/src/lib.rs
@@ -3,3 +3,7 @@ mod utils;
 
 pub use types::{default_config, Command, Config, Session, Tmux, Window};
 pub use utils::{read_config, write_config};
+
+// TODO: look into changing config to YAML
+// TODO: add support for either "local.config.toml" over `config.toml` if present, or adding some
+// sort of #include like system to the config file
