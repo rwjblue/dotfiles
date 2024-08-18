fe800d54f99bad8a52bb849a0a163327a3598cfe
Robert Jackson <me@rwjblue.com>
2024-08-18 10:44:00 -0400

    Remove completed todos

--- binutils/crates/config/src/lib.rs
+++ binutils/crates/config/src/lib.rs
@@ -6,7 +6,3 @@ pub use utils::{read_config, write_config};
 
 // TODO: add support for either "local.config.toml" over `config.toml` if present, or adding some
 // sort of #include like system to the config file
-// TODO: add support for specifying the default session to connect to when `startup-tmux --attach`
-// is ran
-// TODO: add support within a tmux window to specify environment variables (can be set with `tmux
-// new-window -e FOO=bar`)
