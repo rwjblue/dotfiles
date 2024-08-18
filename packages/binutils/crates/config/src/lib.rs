8e45c48690a6b977290d49e957cc385463147e0a
Robert Jackson <me@rwjblue.com>
2024-08-17 20:11:20 -0400

    Remove completed TODO

--- packages/binutils/crates/config/src/lib.rs
+++ packages/binutils/crates/config/src/lib.rs
@@ -4,7 +4,6 @@ mod utils;
 pub use types::{default_config, Command, Config, Session, Tmux, Window};
 pub use utils::{read_config, write_config};
 
-// TODO: look into changing config to YAML
 // TODO: add support for either "local.config.toml" over `config.toml` if present, or adding some
 // sort of #include like system to the config file
 // TODO: add support for specifying the default session to connect to when `startup-tmux --attach`
