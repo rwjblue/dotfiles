3919775274a201ea77adc10ddf5562d8dae78a97
Robert Jackson <me@rwjblue.com>
2024-08-19 22:18:02 -0400

    Swap to `.lua` based config

---
+++ binutils/config/config.lua
@@ -0,0 +1,38 @@
+return {
+  shell_caching = {
+    source = "~/src/rwjblue/dotfiles/zsh/",
+    destination = "~/src/rwjblue/dotfiles/zsh/dist/"
+  },
+  tmux = {
+    sessions = {
+      {
+        name = "✅ todos",
+        windows = {
+          {
+            name = "todos"
+          }
+        }
+      },
+      {
+        name = "dotfiles",
+        windows = {
+          {
+            name = "dotvim",
+            path = "~/src/rwjblue/dotvim",
+            command = "nvim"
+          },
+          {
+            name = "dotfiles",
+            path = "~/src/rwjblue/dotfiles",
+            command = "nvim"
+          },
+          {
+            name = "shared_binutils",
+            path = "~/src/malleatus/shared_binutils",
+            command = "nvim"
+          }
+        }
+      }
+    }
+  }
+}
