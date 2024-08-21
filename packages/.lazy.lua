40b926c34af5012237b6be7593e639b24234be57
Robert Jackson <rjackson@linkedin.com>
2024-08-21 11:39:37 -0400

    Automatically load `Config` types from `malleatus/shared_binutils`

---
+++ packages/.lazy.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        "~/src/malleatus/shared_binutils/config"
+      },
+    },
+  },
+}
--- packages/binutils/config/config.lua
+++ packages/binutils/config/config.lua
@@ -1,29 +1,3 @@
----@class Config
----@field tmux Tmux|nil # Optional tmux configuration
----@field shell_caching ShellCache|nil # Optional shell caching configuration
----@field crate_locations string[]|nil # Optional list of crate locations
-
----@class ShellCache
----@field source string # Source path
----@field destination string # Destination path
-
----@class Tmux
----@field sessions Session[] # List of tmux sessions
----@field default_session string|nil # Default session name (optional)
-
----@class Session
----@field name string # Name of the session
----@field windows Window[] # List of windows in the session
-
----@class Window
----@field name string # Name of the window
----@field path string|nil # Working directory path (optional)
----@field command Command|nil # Command to run in the window (optional)
----@field env table<string, string>|nil # Environment variables (optional)
----@field linked_crates string[]|nil # Linked crates (optional)
-
----@alias Command string|string[] # A single command as a string or multiple commands as a list of strings
-
 ---@type Config
 return {
   shell_caching = {
