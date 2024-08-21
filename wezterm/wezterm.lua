c854a5a8c4fe9f32c728215cac1ff965060a778b
Robert Jackson <rjackson@linkedin.com>
2024-08-21 14:00:51 -0400

    Setup CMD+Enter to toggle full screen

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,5 +6,16 @@ config.color_scheme = 'Tokyo Night'
 config.font = wezterm.font({ family = 'Berkeley Mono', weight = "Bold" })
 config.font_size = 12.0
 config.window_decorations = 'RESIZE'
+config.hide_tab_bar_if_only_one_tab = true
+
+-- native fullscreen is SOOOO slow (the animation kills me)
+config.native_macos_fullscreen_mode = false
+config.keys = {
+  {
+    key = 'Enter',
+    mods = 'CMD',
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
 
 return config
