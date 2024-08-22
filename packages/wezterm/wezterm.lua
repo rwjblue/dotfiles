2c1c571bf706bb1a77be53f785f646be01d66103
Robert Jackson <rjackson@linkedin.com>
2024-08-22 18:15:17 -0400

    Try to tweak wezterm config (difference for retina vs non-retina)

--- packages/wezterm/wezterm.lua
+++ packages/wezterm/wezterm.lua
@@ -2,13 +2,31 @@ local wezterm = require 'wezterm'
 local act = wezterm.action
 local config = wezterm.config_builder()
 
+local function is_retina()
+  for _, monitor in ipairs(wezterm.gui.screens()) do
+    if monitor.dpi > 100 then
+      return true
+    end
+  end
+  return false
+end
+
+
 config.color_scheme = 'Tokyo Night'
 
 -- TODO: figure out if we are being displayed on a retina screen, and use Regular instead of Bold here
 config.font = wezterm.font({ family = 'Berkeley Mono', weight = "Bold" })
 config.font_size = 12.0
+
 config.window_decorations = 'RESIZE'
 config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = { left = 2, right = 2, top = 2, bottom = 2 }
+config.dpi = 72 -- this is the DPI for my widescreen monitor
+--config.dpi = wezterm.gui.get_dpi_for_monitor(wezterm.gui.screens().active)
+
+-- FIXME: calling wezterm.gui.screens() within config seems to make wezterm unresponsive
+--local screens = wezterm.gui.screens()
+
 
 -- native fullscreen is SOOOO slow (the animation kills me)
 config.native_macos_fullscreen_mode = false
@@ -38,6 +56,11 @@ config.keys = {
 }
 
 -- use this along with Ctrl-Shift-L to see the debug messages for what actual keycodes were detected
-config.debug_key_events = true
+--config.debug_key_events = true
+
+-- ideally we can set this based on the current monitor, but since we can't
+-- access wezterm.gui.screen() at the moment in the config just hard code :/
+config.initial_rows = 90
+config.initial_cols = 300
 
 return config
