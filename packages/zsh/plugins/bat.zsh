05dd52429da239caee40f461f4709e9db77bf8e4
Robert Jackson <rjackson@linkedin.com>
2024-09-09 11:40:28 -0400

    Remove `sheldon` usage

--- packages/zsh/plugins/bat.zsh
+++
@@ -1,2 +0,0 @@
-export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-
--- packages/zsh/plugins/ssh.zsh
+++
@@ -1,6 +0,0 @@
-# ensure we use a stable SSH_AUTH_SOCK (so reconnecting to prior tmux sessions
-# continue to be able to auth)
-if [[ -r $HOME/.ssh/ssh_auth_sock ]]; then
-  export SSH_AUTH_SOCK=$HOME/.ssh/ssh_auth_sock
-fi
-
