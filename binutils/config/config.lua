749c73a5440e23b821fad17dbcff35d4e4987990
Robert Jackson <me@rwjblue.com>
2025-02-06 13:56:45 -0500

    fix: Duplicate window name in tmux config

--- binutils/config/config.lua
+++ binutils/config/config.lua
@@ -29,7 +29,7 @@ return {
             command = "nvim"
           },
           {
-            name = "dotvim",
+            name = "common.nvim",
             path = "~/src/malleatus/common.nvim",
             command = "nvim"
           },
