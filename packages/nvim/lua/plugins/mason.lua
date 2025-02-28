01a7fdc4c8b3c1e8bb16725fab8f009c5a1b757e
Robert Jackson <me@rwjblue.com>
2025-02-28 13:43:07 -0500

    feat(nvim): Add mason-extra-cmds to create a MasonUpdateAll command

--- packages/nvim/lua/plugins/mason.lua
+++ packages/nvim/lua/plugins/mason.lua
@@ -1,6 +1,19 @@
 return {
   {
     "williamboman/mason.nvim",
+    -- adds MasonUpdateAll
+    dependencies = { "Zeioth/mason-extra-cmds", opts = {} },
+
+    cmd = {
+      "Mason",
+      "MasonInstall",
+      "MasonUninstall",
+      "MasonUninstallAll",
+      "MasonLog",
+      "MasonUpdate",
+      "MasonUpdateAll", -- this cmd is provided by mason-extra-cmds
+    },
+
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         -- add any additional tools here
