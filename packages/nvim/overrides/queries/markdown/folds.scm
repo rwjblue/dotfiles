4a54a907aa08c76b6e7fbdefe9e169a220139b37
Robert Jackson <me@rwjblue.com>
2025-03-26 16:35:11 -0400

    refactor(nvim): improve markdown folding rules for lists and code blocks
    - Replace unconditional list folding with smarter rule that only folds nested lists with 3+ items
    - Add character count condition (100+) to only fold longer code blocks

--- packages/nvim/overrides/queries/markdown/folds.scm
+++ packages/nvim/overrides/queries/markdown/folds.scm
@@ -1,24 +1,30 @@
 ;; unconditional folding -- ALWAYS consider headers
-;; foldable
-([
-  (section)
-] @fold
-  (#trim! @fold))
-
-;; folding only if the content is "long enough"
-([
-  (fenced_code_block)
-  (indented_code_block)
-  (list_item
-    (list))
-] @fold
-  (#trim! @fold))
+ ;; foldable
+ ([
+   (section)
+ ] @fold
+   (#trim! @fold))
 
 
-;; the default folds.scm has this, but I never want to fold lists in the root
-;; of a section
-;;(section
-;;  (list) @fold
-;;  (#trim! @fold))
+ ;; fold nested lists with at least 3 items
+ ([
+   (list_item
+     (list
+       (list_item) (list_item) (list_item) (_)*))
+ ] @fold
+   (#trim! @fold))
+
+ ;; fold code blocks of 100+ characters
+ ([
+   (fenced_code_block)
+   (indented_code_block)
+ ] @fold
+   (#trim! @fold)
+   (#match? @fold ".{100}"))
 
 
+ ;; the default folds.scm has this, but I never want to fold lists in the root
+ ;; of a section
+ ;;(section
+ ;;  (list) @fold
+ ;;  (#trim! @fold))
