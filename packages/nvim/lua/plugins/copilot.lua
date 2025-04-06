1058496612764d27066d2cb70ade1fb7b324d471
Robert Jackson <me@rwjblue.com>
2025-04-06 12:05:21 -0700

    fix(nvim: copilot): work around LazyVim issue with copilot.lua refactor
    Temporarily work around https://github.com/LazyVim/LazyVim/issues/5899

--- packages/nvim/lua/plugins/copilot.lua
+++ packages/nvim/lua/plugins/copilot.lua
@@ -1,5 +1,14 @@
 return {
   {
+    -- FIXME: work around https://github.com/LazyVim/LazyVim/issues/5899
+    -- Remove this whole section when the issue is resolved in LazyVim
+    "zbirenbaum/copilot.lua",
+    optional = true,
+    opts = function()
+      require("copilot.api").status = require("copilot.status")
+    end,
+  },
+  {
     "CopilotC-Nvim/CopilotChat.nvim",
     optional = true,
     opts = {
@@ -12,7 +21,7 @@ return {
         border = "rounded",
         width = 0.6,
         height = 0.6,
-      }
-    }
-  }
+      },
+    },
+  },
 }
