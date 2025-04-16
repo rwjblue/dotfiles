aaca36c5498ceaa10b43b07e303196fe20e914f7
Robert Jackson <me@rwjblue.com>
2025-04-16 10:40:09 -0400

    chore(nvim): Refactor mcphub.nvim config
    - No need to make it lazier (MCPHub.nvim as of
      [4.7.0](https://github.com/ravitemer/mcphub.nvim/discussions/85)
      handles this automatically)
    - Remove more config that seems to be the default values

--- packages/nvim/lua/plugins/mcp-hub.lua
+++ packages/nvim/lua/plugins/mcp-hub.lua
@@ -1,13 +1,3 @@
-local group = vim.api.nvim_create_augroup("CodeCompanionHooks_MCPHub", {})
-
-vim.api.nvim_create_autocmd({ "User" }, {
-  pattern = "CodeCompanionChatCreated",
-  group = group,
-  callback = function(request)
-    require("mcphub")
-  end,
-})
-
 return {
   {
     "ravitemer/mcphub.nvim",
@@ -17,16 +7,8 @@ return {
     cmd = "MCPHub",
     build = "mise up npm:mcp-hub@latest",
     opts = {
-      port = 9090, -- Port for MCP Hub server
       cmd = vim.fn.expand("~/.local/share/mise/shims/mcp-hub"),
       config = vim.fn.expand("~/.config/mcp/servers.json"),
-      shutdown_delay = 0, -- Wait 0ms before shutting down server after last client exits
-      log = {
-        level = vim.log.levels.WARN,
-        to_file = false,
-        file_path = nil,
-        prefix = "MCPHub",
-      },
     },
   },
 }
