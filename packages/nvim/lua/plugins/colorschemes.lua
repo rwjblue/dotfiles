218c8f28a41b3c2014dc4df3742a73b265e06f76
Robert Jackson <me@rwjblue.com>
2025-08-13 12:50:53 -0400

    feat(nvim): Use Catppuccin colorscheme

--- packages/nvim/lua/plugins/colorschemes.lua
+++ packages/nvim/lua/plugins/colorschemes.lua
@@ -1,19 +1,26 @@
 return {
-  -- add tokyonight
+  -- add catppuccin
   {
-    "folke/tokyonight.nvim",
+    "catppuccin/nvim",
+    name = "catppuccin",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+    },
   },
+
+  -- add tokyonight
+  { "folke/tokyonight.nvim" },
 
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
 
-  -- Configure LazyVim to load gruvbox
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "gruvbox",
-  --   },
-  -- },
+  -- Configure LazyVim to load catppuccin-mocha
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
 }
