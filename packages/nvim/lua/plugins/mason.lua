828be92147330655fdce546535d3d1c527136751
Robert Jackson <me@rwjblue.com>
2025-09-18 17:16:26 -0400

    chore(nvim): Update Zeoith/mason-extra-cmds to support mason.nvim 2.x

--- packages/nvim/lua/plugins/mason.lua
+++ packages/nvim/lua/plugins/mason.lua
@@ -1,9 +1,5 @@
 return {
-  -- NOTE: pin mason related pcakges to ^1.x while the various LazyVim internal
-  -- features are updated to support 2.x
-  -- https://github.com/LazyVim/LazyVim/issues/6039
-  -- https://github.com/LazyVim/LazyVim/pull/6053
-  { "Zeioth/mason-extra-cmds", version = "^1.0.0" },
+  { "Zeioth/mason-extra-cmds" },
 
   {
     "mason-org/mason.nvim",
