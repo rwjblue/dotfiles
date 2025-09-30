19d52104b217f1d903ac009ea571d9575cf56e0a
Robert Jackson <me@rwjblue.com>
2025-09-30 12:19:31 -0400

    Revert "fix(nvim): resolve catppuccin bufferline integration issue"
    This reverts commit 9d7c2406d93204076d3ef8287366a0116eda9f04.

--- packages/nvim/lua/plugins/colorschemes.lua
+++ packages/nvim/lua/plugins/colorschemes.lua
@@ -1,21 +1,13 @@
 return {
-  -- add catppuccin with fixed bufferline integration
+  -- add catppuccin
   {
     "catppuccin/nvim",
     name = "catppuccin",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    opts = function(_, opts)
-      -- Workaround lazyvim issue with catppuccin + bufferline:
-      -- https://github.com/LazyVim/LazyVim/issues/6355
-      local module = require("catppuccin.groups.integrations.bufferline")
-      if module then
-        module.get = module.get_theme
-      end
-      return vim.tbl_deep_extend("force", opts or {}, {
-        flavour = "mocha", -- latte, frappe, macchiato, mocha
-      })
-    end,
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+    },
   },
 
   -- add tokyonight
