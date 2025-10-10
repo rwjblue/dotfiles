8f0e50714a1fa74f894c2831a948c6a6463dee84
Robert Jackson <me@rwjblue.com>
2025-10-10 15:51:22 -0400

    Add Harpoon 2 with snacks.nvim picker integration

---
+++ packages/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,62 @@
+-- Harpoon 2: Quick file navigation
+-- https://github.com/ThePrimeagen/harpoon/tree/harpoon2
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "folke/snacks.nvim",
+  },
+  opts = {
+    settings = {
+      save_on_toggle = true,
+      sync_on_ui_close = true,
+    },
+  },
+  keys = {
+    {
+      -- Add current file to harpoon
+      "<leader>ha",
+      function()
+        require("harpoon"):list():add()
+        vim.notify("Added to Harpoon", vim.log.levels.INFO)
+      end,
+      desc = "Harpoon: Add file",
+    },
+    {
+      -- Open snacks picker with harpoon files
+      "<leader>hh",
+      function()
+        local harpoon = require("harpoon")
+        local harpoon_files = harpoon:list()
+        local items = {}
+
+        for idx, item in ipairs(harpoon_files.items) do
+          table.insert(items, {
+            text = item.value,
+            file = item.value,
+            idx = idx,
+          })
+        end
+
+        Snacks.picker.pick({
+          items = items,
+          format = "file",
+          title = "Harpoon Files",
+          preview = "file",
+          show_empty = true,
+        })
+      end,
+      desc = "Harpoon: Search files",
+    },
+    {
+      -- Remove current file from harpoon
+      "<leader>hr",
+      function()
+        require("harpoon"):list():remove()
+        vim.notify("Removed from Harpoon", vim.log.levels.INFO)
+      end,
+      desc = "Harpoon: Remove file",
+    },
+  },
+}
